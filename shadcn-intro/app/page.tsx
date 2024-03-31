e7af18470080e0904183b8d33c2df7c6300ae887
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-03-31 11:17:31 +0530

    seting up shadcn-intro project

---
+++ shadcn-intro/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <div className="hover:scale-105 hover:duration-300">
+        Shadcn-intro project
+      </div>
+    </div>
+  );
+}
