90cf9d59972b8ac7ca07149e0c8e823de81924ad
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-03-31 12:25:20 +0530

    button

---
+++ shadcn-intro/app/chai/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function ChaiPage() {
+  return (
+    <div className='flex flex-col justify-center items-center  h-screen'>
+    <div>ChaiPage ☕</div>
+    <Button variant={'white'}>Chai Button</Button>
+    </div>
+  )
+}
+
+export default ChaiPage
