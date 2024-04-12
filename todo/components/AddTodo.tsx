d26812d2373ceb5ed6580f86ac070e9590046867
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-13 03:19:34 +0530

    add toast message for toast creation

--- todo/components/AddTodo.tsx
+++ todo/components/AddTodo.tsx
@@ -3,24 +3,32 @@ import { Button } from "@/components/ui/button"
 import { Input } from "./ui/input"
 import { useState } from "react"
 import { addTodo } from "@/actions/addTodo"
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from 'next/navigation'
 
 export function AddTodo(){
     const[todo,setTodo] = useState("")
+    const { toast } = useToast()
+    const router = useRouter()
 
     async function handleClick(){
-        console.log("todo new", todo)
-        console.log("button clicked")
-        await addTodo(todo)
+        toast({
+            variant:"default",
+            description: "Todo created successfully",
+        })
+        
+        console.log("new todo", todo)
+        const res = await addTodo(todo)
+        console.log(res)
+        router.refresh()
     }
 
     return (
-        <div>
-            <form className="flex flex-col items-center gap-2 justify-center">
+        <div className="flex flex-col items-center gap-2 justify-center">
                 <Input placeholder="write todo" onChange={(e)=>{
                     setTodo(e.target.value)
                 }}/>
                 <Button onClick={handleClick} className="focus:scale-95 focus:duration-300">Add Todo</Button>
-            </form>
         </div>
     )
 }
